300dedeeb11715bdc094836f9afc73fb45ce8695
Giuliano Peraz <giuliano.peraz@gmail.com>
2018-08-23 11:10:39 +0200

    Added enum Status to CitybikesStation
    It is useful for asking to the station the status of the free bikes or
    the available places based on thresholds set in the constructor.
    It has been though when the station has to be added to the map with a
    color different for the status of the station, i.e. red for NO_FREE,
    yellow for LOW_FREE and green for MEDIUM_FREE.

--- BikeDaCity/app/src/main/java/it/unipi/iet/bikedacity/CityBikesStation.java
+++ BikeDaCity/app/src/main/java/it/unipi/iet/bikedacity/CityBikesStation.java
@@ -1,6 +1,7 @@
 package it.unipi.iet.bikedacity;
 
 
+import android.location.Location;
 import android.util.Log;
 
 import org.joda.time.DateTime;
@@ -8,22 +9,43 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class CityBikesStation {
+
     private static final String TAG = "CityBikesStation";
+    private final int lowThreshold;
+    private final int mediumThreshold;
+    private Location location;
     private String name;
     private DateTime timestamp;
-    private double longitude;
-    private double latitude;
     private int freeBikes;
     private int emptySlots;
     private String description;
     private Boolean isOnline;
 
+    public enum Status {
+        NO_FREE,
+        NO_AVAILABLE,
+        LOW_FREE,
+        LOW_AVAILABLE,
+        MEDIUM_FREE,
+        MEDIUM_AVAILABLE,
+        HIGH_FREE,
+        HIGH_AVAILABLE
+    }
+
+    private Status freeStatus;
+    private Status availableStatus;
+
     public CityBikesStation(JSONObject jsonObject) throws JSONException {
         name = jsonObject.getString("name");
-        longitude = jsonObject.getDouble("longitude");
-        latitude = jsonObject.getDouble("latitude");
+        double longitude = jsonObject.getDouble("longitude");
+        double latitude = jsonObject.getDouble("latitude");
+        location = new Location("CityBikes");
+        location.setLongitude(longitude);
+        location.setLatitude(latitude);
         freeBikes = jsonObject.getInt("free_bikes");
         emptySlots = jsonObject.getInt("empty_slots");
+        lowThreshold = Math.min(2, (freeBikes + emptySlots) >> 3);
+        mediumThreshold = Math.min(4, (freeBikes + emptySlots) >> 2);
         JSONObject extra;
         try {
             extra = (JSONObject) jsonObject.get("extra");
@@ -38,6 +60,38 @@ public class CityBikesStation {
         timestamp = new DateTime(jsonObject.getString("timestamp"));
     }
 
+    public Status getFreeStatus () {
+        if (freeBikes == 0){
+            freeStatus = Status.NO_FREE;
+        }
+        else if (freeBikes <= lowThreshold){
+            freeStatus = Status.LOW_FREE;
+        }
+        else if (freeBikes <= mediumThreshold){
+            freeStatus = Status.MEDIUM_FREE;
+        }
+        else {
+            freeStatus = Status.HIGH_FREE;
+        }
+        return freeStatus;
+    }
+
+    public Status getAvailableStatus () {
+        if (emptySlots == 0){
+            availableStatus = Status.NO_AVAILABLE;
+        }
+        else if (emptySlots <= lowThreshold){
+            availableStatus = Status.LOW_AVAILABLE;
+        }
+        else if (emptySlots <= mediumThreshold){
+            availableStatus = Status.MEDIUM_AVAILABLE;
+        }
+        else {
+            availableStatus = Status.HIGH_AVAILABLE;
+        }
+        return availableStatus;
+    }
+
     public String getName () {
         return name;
     }
@@ -46,12 +100,8 @@ public class CityBikesStation {
         return timestamp;
     }
 
-    public double getLongitude () {
-        return longitude;
-    }
-
-    public double getLatitude () {
-        return latitude;
+    public Location getLocation (){
+        return location;
     }
 
     public int getFreeBikes () {
