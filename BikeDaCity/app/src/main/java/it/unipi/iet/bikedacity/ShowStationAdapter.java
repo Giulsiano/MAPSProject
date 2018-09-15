a722e89a6076ed5d9565498480f9cb16d6e8f361
Giuliano Peraz <giuliano.peraz@gmail.com>
2018-09-15 09:57:46 +0200

    New layout and strings for the infobox and the station list

--- BikeDaCity/app/src/main/java/it/unipi/iet/bikedacity/ShowStationAdapter.java
+++ BikeDaCity/app/src/main/java/it/unipi/iet/bikedacity/ShowStationAdapter.java
@@ -1,26 +1,31 @@
 package it.unipi.iet.bikedacity;
 
 import android.content.Context;
+import android.content.res.Resources;
+import android.location.Location;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Locale;
 import java.util.TreeMap;
 
 public class ShowStationAdapter extends RecyclerView.Adapter<ShowStationAdapter.StationViewHolder>{
 
     private TreeMap<Float, List<CityBikesStation>> dataSource;
     private List<Station> stations;
+    private MainActivity mainActivity;
+    private Button viewOnMap;
 
-    public ShowStationAdapter (TreeMap<Float, List<CityBikesStation>> distanceMap){
+    public ShowStationAdapter (MainActivity ma, TreeMap<Float, List<CityBikesStation>> distanceMap){
         dataSource = distanceMap;
         stations = null;
+        this.mainActivity = ma;
     }
 
     private class Station {
@@ -74,9 +79,33 @@ public class ShowStationAdapter extends RecyclerView.Adapter<ShowStationAdapter.
         if (stations == null){
             stations = createStationList();
         }
-        Station station = stations.get(position);
-        holder.stationDistance.setText(String.format(Locale.getDefault(), "%f", station.getDistance()));
-        holder.stationName.setText(station.getCityBikeStation().getName());
+
+        // Fill textviews with informations
+        final Station station = stations.get(position);
+        Resources res = mainActivity.getResources();
+        String distance = res.getString(R.string.station_list_distance, station.getDistance());
+        String name = station.getCityBikeStation().getName();
+        String availability;
+        if (mainActivity.isShowingAvailablePlaces()){
+            availability = res.getString(R.string.station_list_availability,
+                                         station.getCityBikeStation().getEmptySlots(),
+                                         res.getString(R.string.station_list_free_places));
+        }
+        else {
+            availability = res.getString(R.string.station_list_availability,
+                                         station.getCityBikeStation().getFreeBikes(),
+                                         res.getString(R.string.station_list_free_bikes));
+        }
+        String details = res.getString(R.string.station_list_details, distance, availability);
+        holder.stationDetail.setText(details);
+        holder.stationName.setText(name);
+        holder.viewOnMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View v) {
+                Location location = station.getCityBikeStation().getLocation();
+                mainActivity.centreMapOn(location.getLatitude(), location.getLongitude());
+            }
+        });
     }
 
     @Override
@@ -87,14 +116,16 @@ public class ShowStationAdapter extends RecyclerView.Adapter<ShowStationAdapter.
         return stations.size();
     }
 
-    public static class StationViewHolder extends RecyclerView.ViewHolder{
+    public static class StationViewHolder extends RecyclerView.ViewHolder {
         private TextView stationName;
-        private TextView stationDistance;
+        private TextView stationDetail;
+        private Button viewOnMap;
 
         public StationViewHolder (View itemView) {
             super(itemView);
             stationName = itemView.findViewById(R.id.station_name);
-            stationDistance = itemView.findViewById(R.id.station_distance);
+            stationDetail = itemView.findViewById(R.id.station_details);
+            viewOnMap = itemView.findViewById(R.id.view_on_map);
         }
     }
 }
