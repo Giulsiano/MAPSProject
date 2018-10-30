07f7413b12d5e74f75959f44e043c039e20175f6
Giuliano Peraz <giuliano.peraz@gmail.com>
2018-10-30 11:40:23 +0100

    Added code for use the overlay inside the map.
    Make use of the reference lastOverlay that make easier for the user to
    make changes at a single overlay without passing its name every time in
    every function.
    Added several functions for controlling the visibility of an overlay or
    a list of overlays.
    Made a single ItemizedIconOverlay.OnItemGestureListener which is called
    rather frequently inside the code.

--- BikeDaCity/app/src/main/java/it/unipi/iet/bikedacity/StationMapManager.java
+++ BikeDaCity/app/src/main/java/it/unipi/iet/bikedacity/StationMapManager.java
@@ -13,6 +13,7 @@ import org.osmdroid.views.overlay.ItemizedIconOverlay;
 import org.osmdroid.views.overlay.Overlay;
 import org.osmdroid.views.overlay.OverlayItem;
 
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
@@ -22,22 +23,44 @@ import java.util.Map;
 public class StationMapManager {
     public static final String TAG = "StationMapManager";
     private static final String MY_CURRENT_LOCATION_TITLE = "I'm here!";
+    public static final long DEFAULT_ANIMATION_DURATION = 500L;
+    public static final double DEFAULT_ZOOM = 18.0;
+    public static final String DEFAULT_OVERLAY = "default";
+    private final String CURRENT_LOCATION_OVERLAY = "CurrentLocation";
 
     private MapView map;
     private Context context;
     private OverlayItem currentMarkerLocation;
-    private ItemizedIconOverlay<OverlayItem> stationOverlay;
+    private ItemizedIconOverlay<OverlayItem> lastOverlay;
+    private Map<String, ItemizedIconOverlay<OverlayItem>> overlayMap;
+    private ItemizedIconOverlay.OnItemGestureListener<OverlayItem> defaultGestureListener;
 
-    public StationMapManager (Context ctx, MapView map){
+    public StationMapManager (Context ctx, final MapView map){
         this.context = ctx;
         this.map = map;
         this.map.setTileSource(TileSourceFactory.DEFAULT_TILE_SOURCE);
         this.map.setTilesScaledToDpi(true);
         this.map.setBuiltInZoomControls(true);
         this.map.setMultiTouchControls(true);
+        this.overlayMap = new HashMap<>();
+        this.lastOverlay = null;
+        this.defaultGestureListener = new ItemizedIconOverlay.OnItemGestureListener<OverlayItem>(){
+            @Override
+            public boolean onItemSingleTapUp (int index, OverlayItem item) {
+                IMapController controller = map.getController();
+                controller.animateTo(item.getPoint(), DEFAULT_ZOOM, DEFAULT_ANIMATION_DURATION);
+                Toast.makeText(context, item.getSnippet(), Toast.LENGTH_LONG).show();
+                return true;
+            }
+
+            @Override
+            public boolean onItemLongPress (int index, OverlayItem item) {
+                return true;
+            }
+        };
     }
 
-    public MapView getMap () {
+    public MapView getMapView () {
         return map;
     }
 
@@ -47,6 +70,30 @@ public class StationMapManager {
 
     public void onResume (){
         map.onResume();
+    }
+
+    public void removeOverlay (String overlayName){
+        ItemizedIconOverlay<OverlayItem> overlay = overlayMap.get(overlayName);
+        if (overlay == null) return;
+        overlayMap.remove(overlayName);
+        map.getOverlays().remove(overlay);
+    }
+
+    public void changeOverlay (String overlayName){
+        ItemizedIconOverlay<OverlayItem> tempOverlay = overlayMap.get(overlayName);
+        if (tempOverlay == null) return;
+        else lastOverlay = tempOverlay;
+    }
+
+    public void setOverlayVisibility (String overlayName, boolean isVisible){
+        ItemizedIconOverlay<OverlayItem> overlay = overlayMap.get(overlayName);
+        List<Overlay> overlayList = map.getOverlays();
+        if (overlayList.contains(overlay)) {
+            if (!isVisible) overlayList.remove(overlay);
+        }
+        else {
+            if (isVisible) overlayList.add(overlay);
+        }
     }
 
     public List<OverlayItem> buildOverlayItemList (CityBikesStation station, String description,
@@ -60,7 +107,7 @@ public class StationMapManager {
         return items;
     }
 
-    public List<OverlayItem> buildOverlayItemList (List<CityBikesStation> stations, Drawable marker){
+    private List<OverlayItem> buildOverlayItemList (List<CityBikesStation> stations, Drawable marker){
         List<OverlayItem> items = new LinkedList<>();
         for (CityBikesStation station : stations){
             OverlayItem item = new OverlayItem(station.getName(), station.getDescription(),
@@ -72,8 +119,8 @@ public class StationMapManager {
         return items;
     }
 
-    public List<OverlayItem> buildOverlayItemList (Map<CityBikesStation, String> stations,
-                                                   Drawable marker){
+    private List<OverlayItem> buildOverlayItemList (Map<CityBikesStation, String> stations,
+                                                    Drawable marker){
         List<OverlayItem> items = new LinkedList<>();
         for (CityBikesStation station : stations.keySet()){
             OverlayItem item = new OverlayItem(station.getName(), stations.get(station),
@@ -91,22 +138,7 @@ public class StationMapManager {
         List<OverlayItem> items = buildOverlayItemList(stations, marker);
 
         // Create the overlay which will show the list of items built above
-        ItemizedIconOverlay<OverlayItem> overlay = new ItemizedIconOverlay<>(items,
-                new ItemizedIconOverlay.OnItemGestureListener<OverlayItem>() {
-                    @Override
-                    public boolean onItemSingleTapUp (int index, OverlayItem item) {
-                        IMapController controller = map.getController();
-                        controller.setCenter(item.getPoint());
-                        Toast.makeText(context, item.getSnippet(), Toast.LENGTH_LONG).show();
-                        return true;
-                    }
-
-                    @Override
-                    public boolean onItemLongPress (int index, OverlayItem item) {
-                        return false;
-                    }
-                }, context);
-        return overlay;
+        return new ItemizedIconOverlay<>(items, defaultGestureListener, context);
     }
 
     private ItemizedIconOverlay<OverlayItem> createStationOverlay (Map<CityBikesStation, String> stations,
@@ -114,56 +146,65 @@ public class StationMapManager {
         List<OverlayItem> items = buildOverlayItemList(stations, marker);
 
         // Create the overlay which will show the list of items built above
-        ItemizedIconOverlay<OverlayItem> overlay = new ItemizedIconOverlay<>(items,
-                new ItemizedIconOverlay.OnItemGestureListener<OverlayItem>() {
-                    @Override
-                    public boolean onItemSingleTapUp (int index, OverlayItem item) {
-                        IMapController controller = map.getController();
-                        controller.setCenter(item.getPoint());
-                        Toast.makeText(context, item.getSnippet(), Toast.LENGTH_SHORT).show();
-                        return true;
-                    }
+        return new ItemizedIconOverlay<>(items, defaultGestureListener, context);
+    }
 
-                    @Override
-                    public boolean onItemLongPress (int index, OverlayItem item) {
-                        return false;
-                    }
-                }, context);
-        return overlay;
+    public void addNamedStationOverlay (String overlayName, Map<CityBikesStation,
+                                        String> stations,
+                                        Drawable marker){
+        List<OverlayItem> itemList = buildOverlayItemList(stations, marker);
+        overlayMap.put(overlayName, new ItemizedIconOverlay<>(itemList, defaultGestureListener, context));
+    }
+
+    public void addNamedStationOverlay (String overlayName) {
+        if (overlayName == null) throw new NullPointerException("overlayName can't be null");
+        overlayMap.put(overlayName, null);
     }
 
     public void addStationMarkers (List<CityBikesStation> stations, Drawable marker){
-        if (stationOverlay == null){
-            stationOverlay = createStationOverlay(stations, marker);
+        if (lastOverlay == null){
+            lastOverlay = createStationOverlay(stations, marker);
         }
         else {
             List<OverlayItem> items = buildOverlayItemList(stations, marker);
-            stationOverlay.addItems(items);
+            lastOverlay.addItems(items);
         }
-        if (!map.getOverlays().contains(stationOverlay)){
-            map.getOverlays().add(stationOverlay);
+        if (!map.getOverlays().contains(lastOverlay)){
+            map.getOverlays().add(lastOverlay);
         }
     }
 
     public void addStationMarkers (Map<CityBikesStation, String> stations, Drawable marker){
-        if (stationOverlay == null){
-            stationOverlay = createStationOverlay(stations, marker);
+        if (lastOverlay == null){
+            lastOverlay = createStationOverlay(stations, marker);
+            overlayMap.put(DEFAULT_OVERLAY, lastOverlay);
         }
         else {
             List<OverlayItem> items = buildOverlayItemList(stations, marker);
-            stationOverlay.addItems(items);
+            lastOverlay.addItems(items);
         }
-        if (!map.getOverlays().contains(stationOverlay)){
-            map.getOverlays().add(stationOverlay);
+        if (!map.getOverlays().contains(lastOverlay)){
+            map.getOverlays().add(lastOverlay);
         }
     }
 
+    public void addStationMarkers (String overlayName, Map<CityBikesStation, String> stations, Drawable marker){
+        // if there are no stations it isn't needed to add them to the overlay
+        if (stations == null) return;
+        lastOverlay = overlayMap.get(overlayName);
+        if (lastOverlay == null) {
+            // overlay doesn't exist, create new one with the name given
+            addNamedStationOverlay(overlayName, stations, marker);
+        }
+        else addStationMarkers(stations, marker);
+    }
+
     public void removeStationMarkers (Iterable<CityBikesStation> stations){
-        if (stationOverlay == null || stationOverlay.size() == 0){
+        if (lastOverlay == null || lastOverlay.size() == 0){
             return;
         }
         Iterator<CityBikesStation> stationIt = stations.iterator();
-        List<OverlayItem> displayedItems = stationOverlay.getDisplayedItems();
+        List<OverlayItem> displayedItems = lastOverlay.getDisplayedItems();
         if (stationIt.hasNext()){
             for (OverlayItem item : displayedItems){
                 if (item.getTitle().equals(stationIt.next().getName())){
@@ -173,22 +214,49 @@ public class StationMapManager {
         }
     }
 
+    public void removeStationMarkers (String overlayName, Iterable<CityBikesStation> stations){
+        lastOverlay = overlayMap.get(overlayName);
+        if (lastOverlay == null) return;
+        removeStationMarkers(stations);
+    }
+
+    public void replaceStationMarkers (String overlayName, Map<CityBikesStation, String> stations, Drawable marker){
+        lastOverlay = overlayMap.get(overlayName);
+        if (lastOverlay == null) {
+            addNamedStationOverlay(overlayName, stations, marker);
+            setOverlayVisibility(overlayName, true);
+        }
+        replaceStationMarkers(stations, marker);
+    }
+
     public void replaceStationMarkers (Map<CityBikesStation, String> stations, Drawable marker){
         if (stations == null) return;
         removeStationMarkers(stations.keySet());
         addStationMarkers(stations, marker);
     }
 
+    public void replaceAllStationMarkers (String overlayName, Map<CityBikesStation, String> newStations,
+                                          Drawable newMarker){
+        lastOverlay = overlayMap.get(overlayName);
+        removeAllMarkers();
+        addStationMarkers(overlayName, newStations, newMarker);
+    }
+
+    public void removeAllMarkers (String overlayName) {
+        lastOverlay = overlayMap.get(overlayName);
+        removeAllMarkers();
+    }
+
     public void removeAllMarkers (){
-        if (stationOverlay == null) return;
-        stationOverlay.removeAllItems();
+        if (lastOverlay == null) return;
+        lastOverlay.removeAllItems();
     }
 
     public void removeStationMarker (CityBikesStation station){
-        if (stationOverlay == null || stationOverlay.size() == 0){
+        if (lastOverlay == null || lastOverlay.size() == 0){
             return;
         }
-        List<OverlayItem> displayedItems = stationOverlay.getDisplayedItems();
+        List<OverlayItem> displayedItems = lastOverlay.getDisplayedItems();
         for (OverlayItem item : displayedItems){
             if (item.getTitle().equals(station.getName())){
                 displayedItems.remove(item);
@@ -206,49 +274,29 @@ public class StationMapManager {
                                                             currentLocation.getLongitude()));
             currentMarkerLocation.setMarker(marker);
         }
-        if (stationOverlay == null){
+        if (lastOverlay == null){
             List<OverlayItem> items = new LinkedList<>();
             items.add(currentMarkerLocation);
-            stationOverlay = new ItemizedIconOverlay<>(items,
-                    new ItemizedIconOverlay.OnItemGestureListener<OverlayItem>() {
-                        @Override
-                        public boolean onItemSingleTapUp (int index, OverlayItem item) {
-                            IMapController controller = map.getController();
-                            controller.animateTo(new GeoPoint(item.getPoint().getLatitude(),
-                                            item.getPoint().getLongitude()),
-                                    18.0,
-                                    500L);
-                            Toast.makeText(context, item.getSnippet(), Toast.LENGTH_SHORT).show();
-                            return true;
-                        }
-
-                        @Override
-                        public boolean onItemLongPress (int index, OverlayItem item) {
-                            return false;
-                        }
-                    }, context);
-            map.getOverlays().add(stationOverlay);
+            lastOverlay = new ItemizedIconOverlay<>(items,defaultGestureListener, context);
+            map.getOverlays().add(lastOverlay);
         }
         else {
-            stationOverlay.addItem(currentMarkerLocation);
+            lastOverlay.addItem(currentMarkerLocation);
         }
-        setMarkerVisibility(true);
+        setOverlayVisibility(true);
     }
 
-    public void moveTo (Location location){
+    public void moveCameraTo (Location location){
         IMapController controller = map.getController();
-
-        // Need to zoom then center due to a bug in OSMDroid
-        controller.animateTo(new GeoPoint(location.getLatitude(),
-                        location.getLongitude()),
-                18.0,
-                500L);
+        controller.animateTo(new GeoPoint(location.getLatitude(), location.getLongitude()),
+                             DEFAULT_ZOOM,
+                             DEFAULT_ANIMATION_DURATION);
     }
 
     public void removeCurrentLocationMarker (){
         if (currentMarkerLocation == null) return;
-        if (stationOverlay == null || stationOverlay.size() == 0) return;
-        stationOverlay.removeItem(currentMarkerLocation);
+        if (lastOverlay == null || lastOverlay.size() == 0) return;
+        lastOverlay.removeItem(currentMarkerLocation);
         currentMarkerLocation = null;
     }
 
@@ -257,19 +305,31 @@ public class StationMapManager {
         addCurrentLocationMarker(currentLocation, marker);
     }
 
-    private void setMarkerVisibility (boolean visibility){
+    private void setOverlayVisibility (boolean isVisible){
         List<Overlay> overlays = map.getOverlays();
-        if (visibility == true){
-            if (!overlays.contains(stationOverlay)){
-                overlays.add(stationOverlay);
+        if (isVisible){
+            if (!overlays.contains(lastOverlay)){
+                overlays.add(lastOverlay);
                 map.invalidate();
             }
         }
         else {
-            if (overlays.contains(stationOverlay)){
-                overlays.remove(stationOverlay);
+            if (overlays.contains(lastOverlay)){
+                overlays.remove(lastOverlay);
                 map.invalidate();
             }
+        }
+    }
+
+    public void setVisibleOverlays (List<String> names){
+        // remove all overlay from the map, then re-add those who the user wants them to show
+        List<Overlay> overlayList = map.getOverlays();
+        for (Overlay overlay : overlayList) overlayList.remove(overlay);
+        for (String overlayName : names){
+            // Get the overlay, if it exist re-add to the visible overlay
+            lastOverlay = overlayMap.get(overlayName);
+            if (lastOverlay == null) continue;
+            setOverlayVisibility(overlayName, true);
         }
     }
 }
